907e2b7ae9d6b9cefa9fcd806aa8dd2841231483
ikbeomjeon.home <jeonikbeom@gmail.com>
2023-08-18 16:45:54 +0900

    add button

--- Assets/Scripts/Editor/MainWindow.cs
+++ Assets/Scripts/Editor/MainWindow.cs
@@ -1,11 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MainWindow : EditorWindow
 {
-    string dataset_path;
+    string scene_path;
+
+    Vector2 scrollPos = Vector2.zero;
+    public bool isLoaded = false;
+    public bool isPlaying = false;
+
+    
+    private void OnGUI()
+    {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
+
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        if (GUILayout.Button("Select scene path"))
+        {
+            var path = EditorUtility.OpenFolderPanel("Select scene path", scene_path, "");
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                scene_path = path;
+                SavePreperence();
+            }
+
+        }
+        EditorGUILayout.LabelField(scene_path, EditorStyles.boldLabel);
+        EditorGUILayout.EndVertical();
+
+        if (!string.IsNullOrEmpty(scene_path))
+        {
+            EditorGUILayout.BeginHorizontal();
+            var playButtonContent = EditorGUIUtility.IconContent("PlayButton On");
+            var stopButtonContent = EditorGUIUtility.IconContent("d_PreMatQuad");
+            var pauseButtonContent = EditorGUIUtility.IconContent("PauseButton");
+            if (!isLoaded && GUILayout.Button(playButtonContent))
+            {
+
+                LoadAll();
+                isLoaded = true;
+                isPlaying = false;
+            }
+
+            else if (isLoaded && GUILayout.Button(stopButtonContent))
+            {
+                DestroyAll();
+                isLoaded = false;
+                isPlaying = false;
+            }
+
+            if (isPlaying)
+            {
+                if (GUILayout.Button(pauseButtonContent))
+                {
+                    //just stop
+                    isPlaying = !isPlaying;
+                }
+            }
+            else
+            {
+                var backup = GUI.backgroundColor;
+                GUI.backgroundColor = Color.cyan;
+                if (GUILayout.Button(pauseButtonContent))
+                {
+                    //just resume
+                    isPlaying = !isPlaying;
+                }
+                GUI.backgroundColor = backup;
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    void LoadAll()
+    {
+        
+    }
+    void DestroyAll()
+    {
+        
+    }
+
+    public void LoadPreperence()
+    {
+        if (PlayerPrefs.HasKey("scene_path"))
+        {
+            scene_path = PlayerPrefs.GetString("scene_path");
+            Debug.Log(scene_path);
+        }
+    }
+
+    public void SavePreperence()
+    {
+        PlayerPrefs.SetString("scene_path", scene_path);
+    }
+
 
     [MenuItem("BOPHelper/MainWindow")]
     public static MainWindow OpenWindow()
@@ -25,7 +121,8 @@ public class MainWindow : EditorWindow
             else
             {
                 instance = GetWindow<MainWindow>(false, "BOPHelper");
-
+                instance.LoadPreperence();
+                
                 return instance;
             }
         }
